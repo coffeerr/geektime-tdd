9bbc3353d584ee9bf7f5f9f7d5fc22b509b6b899
chris <chriso1998@qq.com>
2022-10-05 11:06:29 +0800

    V0.2 01 TDD演示（1）：任务分解法与整体工作流程

---
+++ src/main/java/com/coffeerr/geektimetdd/args/Args.java
@@ -0,0 +1,44 @@
+package com.coffeerr.geektimetdd.args;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: Desmond
+ * @time: 2022/10/5 10:33 AM
+ */
+
+public class Args {
+    public static <T> T parse(Class<T> optionClass, String... args) {
+
+        try {
+            Constructor<?> constructor = optionClass.getDeclaredConstructors()[0];
+            List<String> arguments = Arrays.asList(args);
+            Object[] values = Arrays.stream(constructor.getParameters()).map(it -> parseOption(it, arguments)).toArray();
+            return (T) constructor.newInstance(values);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Object parseOption(Parameter parameter, List<String> arguments) {
+        Object value = null;
+        Option option = parameter.getAnnotation(Option.class);
+        String flag = "-" + option.value();
+        if (parameter.getType() == boolean.class) {
+            value = arguments.contains(flag);
+        }
+        if (parameter.getType() == int.class) {
+            int index = arguments.indexOf(flag);
+            value = Integer.parseInt(arguments.get(index + 1));
+        }
+        if (parameter.getType() == String.class) {
+            int index = arguments.indexOf(flag);
+            value = String.valueOf(arguments.get(index + 1));
+        }
+        return value;
+    }
+}
